8290224ab90c02fef243cc902a6dc194fcc81ce2
lakshmip04 <bl.en.u4cse22034@bl.students.amrita.edu>
2025-03-24 00:11:12 +0530

    generate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,18 @@
-export const cart=[];
+export const cart=[
+    {
+        
+        productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6", 
+        productName:"Black and Gray Athletic Cotton Socks - 6 Pairs",
+        quantity:3
+        
+      },
+      {
+    
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d", 
+        productName:"Intermediate Size Basketball",
+        quantity:5
+      }
+];
 
 //data attribute- is just another html attribute
 //allows us to attach any info to an element
